4de21ea520d86910117547aa2ca750fdbaa83fc2
AXEL <renot9pro@gmail.com>
2024-03-31 16:38:44 +0700

    first commmit

---
+++ README.md
@@ -0,0 +1,2 @@
+tanny
+# tanny
---
+++ vuejs/src/components/TheWelcome.vue
@@ -0,0 +1,77 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>PENDIDIKAN</template>
+
+    Saya mahasiswa di salah satu Kampus swasta di Riau yaitu
+    <a href="https://uir.ac.id/" target="_blank" rel="noopener">Universitas Islam Riau</a>
+    .
+    Sebelumnya saya lulusan dari 
+    <a href="https://dapo.kemdikbud.go.id/sekolah/118758B509FC6FA96A7E" target="_blank" rel="noopener">SMK PENERBANGAN SPAN PEKANBARU</a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Tempat, Tanggal Lahir</template>
+
+    Saya Lahir di kota  <a href="https://maps.app.goo.gl/AuZBkQByxKyoxTkS7" target="_blank" rel="noopener"
+      >Pulau Kecil</a>
+      
+    . Pada tanggal
+     20 April 2002
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Jurusan</template>
+
+    Prodi atau Jurusan Kuliah
+Saya mengambil Program didik Teknik Informatika atau biasa disebut IT
+    , saat ini saya berada di semester 4 Kelas 4A atau yang biasa disebut 
+    <a href="https://www.instagram.com/dobleh_a22/" target="_blank" rel="noopener">Dobleh A22</a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Hobi</template>
+
+    Hobi yang paling saya suka adalah : <br />
+    1.
+    <a href="https://www.viu.com/ott/id/id/category/549/Drama-Korea" target="_blank" rel="noopener"
+      >NONTON DRAKOR</a
+    ><br />
+    2. JALAN SAMA AYANG <br/>
+    3. Makan <br/>
+    </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Social Media</template>
+
+    Saya sering menggunakan soal media untuk berbagi aktivitas keseharian saya,ataupun mencari informasi<br />
+    Instagram = @<a href="https://www.instagram.com/tnnyaprlni/" target="_blank" rel="noopener"
+      >tanny_apriliani</a
+    > <br/>
+
+  </WelcomeItem>
+</template>
